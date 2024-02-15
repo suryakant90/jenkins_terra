235202810275d37275f9060f04c29869e32cc7c6
suryakant90 <62537489+suryakant90@users.noreply.github.com>
2024-02-15 20:58:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    } 
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name="user25_deployer-key"
+  count=3
+  tags = {
+      Name="user25-instance-${count.index}",
+      role=count.index==0?"user25-lb": (count.index<3?"user25-web":"user25-backend")
+  }
+}
+
+output "ips"{
+    value = aws_instance.instance.*.public_ip
+}
